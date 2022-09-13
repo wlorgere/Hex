27d2b729d1c410102c431d78b45993d5482e541b
wlorgere <williamlorgere@gmail.com>
2022-09-13 16:43:02 +0200

    create minmax, it doesn't work :'(

--- AI/minmax.py
+++ AI/minmax.py
@@ -1,5 +1,34 @@
+from copy import deepcopy
 from AI.aiBasicClass import AIBasicClass
+from hex.board import Board
+class Minmax(AIBasicClass):
+    def find_best_move(self, board: Board, turn):
+        def recursion(board: Board, player):
+            winner = board.winner()
 
+            if winner != None:
+                return (-1 if winner == player else 1, None)
 
-class Minimax(AIBasicClass):
-    pass
+            value = float("-inf")
+
+            test = [[0 for _ in range(board.row)] for _ in range(board.col)]
+            for i in range(board.row):
+                for j in range(board.col):
+                    if(board.board[i][j] == "BLACK"):
+                        current_board = deepcopy(board)
+                        current_board.move(i, j, player)
+                        player = "BLUE" if player == "RED" else "RED"
+                        eval, move  = recursion(current_board, player)
+                        eval = - eval
+                        test[i][j] = eval
+                        if eval > value:
+                            value = eval
+                            move = i,j
+
+            return eval, move
+        
+        eval, move = recursion(board, turn)
+
+        
+        return move
+
